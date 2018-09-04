a76bb2f8e5ba2f7642d8b745f3abbe8c2af80224
Nuno Rodrigues <nuno.r.rodrigues@outlook.pt>
2018-09-04 13:31:15 +0100

    Check internet connection

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,26 +1,49 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ConnectionService } from 'ng-connection-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor(private http: HttpClient) { }
+  status = 'CHECK';
+  isConnected = true;
+
+  constructor(private http: HttpClient, private connectionService: ConnectionService) {
+    this.connectionService.monitor().subscribe(isConnected => {
+      this.isConnected = isConnected;
+      if (this.isConnected) {
+        this.status = "ONLINE";
+      }
+      else {
+        this.status = "OFFLINE";
+      }
+    })
+  }
 
   getUsers() {
-    // return this.http.get('https://jsonplaceholder.typicode.com/users')
-    return this.http.get('./assets/users.json/')
+    if (this.status === 'ONLINE'){
+      return this.http.get('https://jsonplaceholder.typicode.com/users')
+    } else {
+      return this.http.get('./assets/users.json/')
+    }
   }
 
   getUser(userId) {
-    return this.http.get('https://jsonplaceholder.typicode.com/users/'+userId)
-    // return this.http.get('./assets/user.json/1')
+    if (this.status === 'ONLINE'){
+      return this.http.get('https://jsonplaceholder.typicode.com/users/'+userId)
+    } else {
+      return this.http.get('./assets/user.json')
+    }
   }
 
   getPosts() {
-    // return this.http.get('https://jsonplaceholder.typicode.com/posts')
-    return this.http.get('./assets/posts.json/')
+    if (this.status === 'ONLINE'){
+      return this.http.get('https://jsonplaceholder.typicode.com/posts')
+    } else {
+      return this.http.get('./assets/posts.json/')
+    }
   }
 
 }
